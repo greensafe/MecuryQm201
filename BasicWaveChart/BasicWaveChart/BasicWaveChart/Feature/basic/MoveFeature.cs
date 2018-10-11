35f4362ef68c295f90f4d21848aee1e80c4bd177
thinkestcrm <waxYZ@gmail.com>
2018-10-11 19:00:44 +0800

    添加移动feautre

---
+++ BasicWaveChart/BasicWaveChart/BasicWaveChart/Feature/basic/MoveFeature.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace featurefactory.Basic
+{
+    class MoveFeature:DependencyObject
+    {
+        //static dynamic target;
+
+        //connector
+        public static readonly DependencyProperty EnableProperty = DependencyProperty.RegisterAttached("Enable", typeof(bool),
+            typeof(MoveFeature), new PropertyMetadata(false, new PropertyChangedCallback(OnEnableChanged))
+            );
+
+        private static void OnEnableChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            //target = d;
+            findParent(d as FrameworkElement).Loaded += delegate (object sender, RoutedEventArgs epr) {
+                MoveWorker.Create(d).Enable();
+            };
+        }
+        public static void SetEnble(DependencyObject d, bool use)
+        {
+            d.SetValue(EnableProperty, use);
+        }
+
+        #region private func
+        static private Window findParent(FrameworkElement me)
+        {
+            while (!(me is Window))
+            {
+                me = me.Parent as FrameworkElement;
+            }
+            return me as Window;
+        }
+        #endregion
+    }
+
+    internal class MoveWorker
+    {
+        dynamic target;
+        dynamic targetcontext = new TargertContext();
+        private bool moving = false;
+
+        private MoveWorker()
+        {
+
+        }
+        static public MoveWorker Create(dynamic target)
+        {
+            MoveWorker worker = new MoveWorker();
+            worker.target = target;
+            return worker;
+        }
+        //enable the feature
+        public void Enable()
+        {
+            targetcontext.container = target.FindName("container") as Canvas;
+            target.MouseLeftButtonDown += new MouseButtonEventHandler(MouseleftdownHdlr);
+            target.MouseLeftButtonUp += new MouseButtonEventHandler(MouseLeftUpHdlr);
+            target.MouseMove += new MouseEventHandler(MouseMoveHdlr);
+        }
+
+        private void MouseMoveHdlr(object sender, MouseEventArgs e)
+        {
+            if (moving == true)
+            {
+
+                Canvas.SetLeft(target, e.GetPosition(targetcontext.container).X -target.ActualWidth / 2);
+                Canvas.SetTop(target, e.GetPosition(targetcontext.container).Y -target.ActualHeight / 2);
+                return;
+            }
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                moving = true;
+                target.CaptureMouse();
+            }
+        }
+
+        private void MouseleftdownHdlr(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void MouseLeftUpHdlr(object sender, MouseButtonEventArgs e)
+        {
+            moving = false;
+            target.ReleaseMouseCapture();
+        }
+    }
+
+    internal class TargertContext : DynamicObject
+    {
+        Dictionary<string, object> dictionary
+            = new Dictionary<string, object>();
+        public int Count
+        {
+            get
+            {
+                return dictionary.Count;
+            }
+        }
+        public override bool TryGetMember(
+            GetMemberBinder binder, out object result)
+        {
+            string name = binder.Name.ToLower();
+            return dictionary.TryGetValue(name, out result);
+        }
+        public override bool TrySetMember(
+            SetMemberBinder binder, object value)
+        {
+            dictionary[binder.Name.ToLower()] = value;
+            return true;
+        }
+    }
+}
---
+++ BasicWaveChart/BasicWaveChart/BasicWaveChart/Feature/integral/IntegralFeature.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace BasicWaveChart.Feature.integral
+{
+    public class IntegralFeature: DependencyObject
+    {
+        private static BasicWaveChartUC hostctl = null;
+        private static dynamic hostcontext = new HostContext();
+
+        public static readonly DependencyProperty EnableProperty = DependencyProperty.RegisterAttached(
+                "Enable", typeof(bool), typeof(IntegralFeature), new PropertyMetadata(false, OnEnableChanged));
+
+        public static void SetEnable(DependencyObject d, bool use)
+        {
+            d.SetValue(EnableProperty, use);
+        }
+        private static void OnEnableChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            BasicWaveChartUC basicwave = d as BasicWaveChartUC;
+            basicwave.Loaded += new RoutedEventHandler(targetloaded_hdlr);
+            ;
+            //throw new NotImplementedException();
+        }
+
+        private static void targetloaded_hdlr(object sender, RoutedEventArgs e)
+        {
+            hostctl = sender as BasicWaveChartUC;        
+            hostcontext.window = hostctl.WindowCanvas;   //canvas
+            hostcontext.container = hostctl.basecanvas;  //canvas
+            Console.WriteLine(hostcontext.window.Width);
+            Console.WriteLine(hostcontext.window.Height);
+        }
+    }
+
+    public class HostContext : DynamicObject
+    {
+        Dictionary<string, object> dictionary
+            = new Dictionary<string, object>();
+        public int Count
+        {
+            get
+            {
+                return dictionary.Count;
+            }
+        }
+        public override bool TryGetMember(
+            GetMemberBinder binder, out object result)
+        {
+            string name = binder.Name.ToLower();
+            return dictionary.TryGetValue(name, out result);
+        }
+        public override bool TrySetMember(
+            SetMemberBinder binder, object value)
+        {
+            dictionary[binder.Name.ToLower()] = value;
+            return true;
+        }
+    }
+
+}
